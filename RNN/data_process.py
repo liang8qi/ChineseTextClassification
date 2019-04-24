790090a6de58f7642b3f1d937395d13f11043012
DrLiLiang <41186261+DrLiLiang@users.noreply.github.com>
2019-04-24 15:38:54 +0800

    RNN Model

---
+++ RNN/data_process.py
@@ -0,0 +1,277 @@
+import re
+import os
+import numpy as np
+import pynlpir
+import random
+import tensorflow.contrib.keras as kr
+
+categories = {
+            "C000008": 0,
+            "C000010": 1,
+            "C000013": 2,
+            "C000014": 3,
+            "C000016": 4,
+            "C000020": 5,
+            "C000022": 6,
+            "C000023": 7,
+            "C000024": 8
+        }
+num_category = 9 # 类别数
+
+
+class Data(object):
+    """清洗训练数据，去除数据中的非中文数字字母字符，并对文件进行分词"""
+
+    @staticmethod
+    def load_stop_words(stop_words_src):
+        stop_words = []
+        with open(stop_words_src, "r", encoding='utf8') as f:
+            lines = f.readlines()
+            for line in lines:
+                stop_words.append(line.strip())
+        return stop_words
+
+    @staticmethod
+    def clean_sentence(sentence):
+        # 抽出中文字符及数字
+        # sentence = re.sub(r"[^\u4e00-\u9fff()a-zA-Z0-9，！？\“\'\：\” ]", " ", sentence)
+        # sentence = re.sub(r"[^\u4e00-\u9fff]", " ", sentence)
+        sentence = re.sub(r"[^\u4E00-\u9FD5]", " ", sentence)
+
+        # 删除两个以上连续空白符
+        # sentence = re.sub(r"\s{2,}", "", sentence).replace(" ", "")
+        # sentence = re.sub(r"\s{2,}", "", sentence)
+
+        return sentence.strip()
+
+    # 处理一个文件
+    def segmentation(self, file_src, output_src, stop_words):
+        sentence_list = []
+
+        with open(file_src, "r", encoding="gbk", errors="ignore") as f:
+            content = f.read()
+            sentences = content.replace('\t', '').replace('\u3000', '').split("\n")
+            for sentence in sentences:
+                sentence = self.clean_sentence(sentence)
+                if len(sentence):
+                    sentence_list.append(sentence)
+
+        if len(sentence_list) == 0:
+            return False
+
+        output_file = open(output_src, "a", encoding="utf-8")
+        sentence_segment = []
+        for sentence in sentence_list:
+            # words = list(jieba.cut(sentence))
+            words = pynlpir.segment(sentence, pos_tagging=False)
+            for word in words:
+                if not len(word):
+                    continue
+                # 去停用词
+                if word not in stop_words:
+                    sentence_segment.append(word)
+        output_file.write(" ".join(sentence_segment))
+        output_file.close()
+        return True
+
+    # 批量处理
+    # 同时保存一个输出文件列表
+    def batch_process(self, dirs_src, output_src, stop_words):
+        if not os.path.exists(output_src):
+            os.mkdir(output_src)
+        data_set = {}
+        dir_list = os.listdir(dirs_src)
+        invalid_files = []
+        pynlpir.open()
+        for dir_name in dir_list:
+            if dir_name not in categories:
+                continue
+
+            output_dir = os.path.join(output_src, dir_name)
+            if os.path.exists(output_dir) is not True:
+                os.mkdir(output_dir)
+
+            dir_src = os.path.join(dirs_src, dir_name)
+            category = categories[dir_name]
+
+            if str(category) not in data_set:
+                data_set[str(category)] = []
+
+            file_list = os.listdir(dir_src)
+            for file in file_list:
+                if file.endswith(".txt") is not True:
+                    continue
+
+                output_file_src = os.path.join(output_dir, file.replace("txt", str(category)))
+                input_file_src = os.path.join(dir_src, file)
+
+                result = self.segmentation(input_file_src, output_file_src, stop_words)
+                if result:
+                    data_set[str(category)].append(output_file_src)
+                    print("{} is cut successfully".format(output_file_src))
+                else:
+                    # print("{} is Null".format(input_file_src))
+                    invalid_files.append(input_file_src)
+
+        pynlpir.close()
+        if len(invalid_files) > 0:
+            print("无效文件:")
+            for file in invalid_files:
+                print(file)
+
+        np.save(os.path.join(output_src, "file_list.npy"), data_set)
+        print("Finish")
+
+    # 划分数据集
+    @staticmethod
+    def divide_data_set(file_list_src, output_src, proportion):
+        if not os.path.exists(output_src):
+            os.mkdir(output_src)
+        data_set = np.load(file_list_src).item()
+        train_data_set = []
+        verify_data_set = []
+        test_data_set = []
+
+        for category in data_set:
+            data_cnt = len(data_set[category])
+            index_list = random.sample(range(data_cnt), data_cnt)
+
+            middle = int(data_cnt * proportion[0])
+            end = int(data_cnt * proportion[1]) + middle
+
+            train_data_set.extend([data_set[category][index] for index in index_list[:middle]])
+            verify_data_set.extend([data_set[category][index] for index in index_list[middle:end]])
+            test_data_set.extend([data_set[category][index] for index in index_list[end:]])
+
+        print("the size of train_dataset is {}".format(len(train_data_set)))
+        print("the size of verify_dataset is {}".format(len(verify_data_set)))
+        print("the size of test_dataset is {}".format(len(test_data_set)))
+
+        np.save(os.path.join(output_src, "train_file_list.npy"), train_data_set)
+        np.save(os.path.join(output_src, "valid_file_list.npy"), verify_data_set)
+        np.save(os.path.join(output_src, "test_file_list.npy"), test_data_set)
+
+        print("Divide Finish")
+
+    # 建立词典
+    @staticmethod
+    def build_vocab(train_file_list_src, vocab_src):
+        train_files = np.load(train_file_list_src)
+        print("the num of train files is {}".format(len(train_files)))
+
+        vocab = set()
+
+        for file in train_files:
+            with open(file, "r", encoding="utf-8") as f:
+                line = f.readline()
+                words = line.strip().split(" ")
+                for word in words:
+                    if not len(word):
+                        continue
+                    vocab.add(word)
+
+        # 添加一个 <PAD> 来将所有文本pad为同一长度
+        vocab = ['<PAD>'] + list(vocab)
+
+        np.save(vocab_src, vocab)
+        print("The size of vocab is {}".format(len(vocab)))
+
+    # 加载词典
+    @staticmethod
+    def load_vocab(vocab_src):
+        vocab = np.load(vocab_src)
+        word_to_id = dict(zip(vocab, range(len(vocab))))
+
+        return vocab, word_to_id
+
+    @staticmethod
+    def text_vectorization(file_src, vocab, word_to_id):
+        text_vector = []
+        label = int(file_src[-1])  # 9类 最后一位为类别
+        text_len = 0
+        with open(file_src, "r", encoding="utf-8") as f:
+            line = f.readline().strip()
+            words = line.split(" ")
+            for word in words:
+                if word in vocab:
+                    text_vector.append(word_to_id[word])
+                    text_len += 1
+
+        print("{} is finished".format(file_src))
+        return text_vector, label, text_len
+
+    # 将一个数据集中的文件向量化
+    def batch_vectorization(self, data_set_src, vocab, word_to_id, output_src):
+        if not os.path.exists(output_src):
+            os.mkdir(output_src)
+
+        data_set = np.load(data_set_src)
+        x_list = []
+        y_list = []
+        text_len_list = []
+        cnt = 0
+        print(len(data_set))
+        for data in data_set:
+            x, y, text_len = self.text_vectorization(data, vocab, word_to_id)
+            x_list.append(x)
+            y_list.append(y)
+            text_len_list.append(text_len)
+            print(cnt)
+            cnt += 1
+
+        # 最大文本长度
+        max_len = np.max(text_len_list)
+        # 将文本pad为固定长度
+        # x_pad = kr.preprocessing.sequence.pad_sequences(x_list, max_len)
+        y_pad = kr.utils.to_categorical(y_list, num_classes=num_category)  # 将标签转换为one-hot表示 9类
+        # 保存以便多次使用
+        np.save(os.path.join(output_src, "x.npy"), x_list)
+        np.save(os.path.join(output_src, "y.npy"), y_pad)
+        np.save(os.path.join(output_src, "text_len.npy"), text_len_list)
+
+        print("the dataset {} vectorization is finished".format(data_set_src))
+
+
+if __name__ == "__main__":
+
+    data = Data()
+    dirs_src = "data/sougoudataset"
+    output_src = "data/pynlpir"
+    stop_words_src = "data/stopwords.txt"
+
+    stop_words = data.load_stop_words(stop_words_src)
+    # 分词
+    # data.batch_process(dirs_src, output_src, stop_words)
+
+    # 划分数据集
+    file_list_src = os.path.join(output_src, "file_list.npy")
+    proportion = [0.7, 0.2]
+    divided_files_list_src = "data/file_list"
+    # data.divide_data_set(file_list_src, divided_files_list_src, proportion)
+
+    # 建立词典
+    vocab_src = "data/middle_result/vocab_small.npy"
+    # data.build_vocab(os.path.join(divided_files_list_src, "train_file_list.npy"), vocab_src)
+
+    # 加载词典
+    vocab, word_to_id = data.load_vocab(vocab_src)
+    print(len(vocab))
+    # 训练集向量化
+    train_src = "data/vectorized_data/train"
+    data.batch_vectorization(os.path.join(divided_files_list_src, "train_file_list.npy"),
+                             vocab, word_to_id, train_src)
+
+    # 验证集向量化
+    validation_src = "data/vectorized_data/validation"
+    data.batch_vectorization(os.path.join(divided_files_list_src, "valid_file_list.npy"),
+                             vocab, word_to_id, validation_src)
+
+    # 测试集向量化
+    test_src = "data/vectorized_data/test"
+    data.batch_vectorization(os.path.join(divided_files_list_src, "test_file_list.npy"),
+                             vocab, word_to_id, test_src)
+
+
+
+
+
---
+++ RNN/text_bilstm_model.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+
+
+class TBiLSTMConfig(object):
+    """RNN配置参数"""
+
+    # 模型参数
+    embedding_dim = 64     # 词向量维度
+    seq_length = 600        # 序列长度
+    num_classes = 9        # 类别数
+    vocab_size = 35809       # 词汇表达小
+
+    num_layers = 2           # 隐藏层层数
+    hidden_dim = 128        # 隐藏层神经元
+
+    dropout_keep_prob = 0.8  # dropout保留比例
+    learning_rate = 1e-3    # 学习率
+
+    batch_size = 128        # 每批训练大小
+    num_epochs = 10          # 总迭代轮次
+
+    print_per_batch = 100    # 每多少轮输出一次结果
+    save_per_batch = 10      # 每多少轮存入tensorboard
+    l2_reg_lambda = 0.01
+    filter_sizes = [1]  # 卷积核大小
+    filter_num = 128  # 卷积核数量
+
+
+class TextBiLSTM(object):
+    """文本分类，RNN模型"""
+    def __init__(self, config):
+        self.config = config
+
+
+        # 三个待输入的数据
+        self.input_x = tf.placeholder(tf.int32, [None, None], name='input_x')
+        self.input_y = tf.placeholder(tf.float32, [None, self.config.num_classes], name='input_y')
+        self.input_text_len = tf.placeholder(tf.int32, [None], name="input_text_len")
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+        self.l2_loss = tf.constant(0.0)
+
+        # embedding
+        # 词向量映射
+        with tf.device('/cpu:0'):
+            embedding = tf.get_variable('embedding', [self.config.vocab_size, self.config.embedding_dim],
+                                        dtype=tf.float32)
+
+            # batch_size, max_len, embed_dim]
+            self.embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_x)
+            print(self.embedding_inputs)
+
+        with tf.name_scope("RNN"):
+            # 双向LSTM
+            lstm_fw_cell = self.dropout()
+
+            lstm_bw_cell = self.dropout()
+
+            outputs, states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, self.embedding_inputs,
+                                                              dtype=tf.float32)
+            # combination = [outputs[0], self.embedding_inputs, outputs[1]]
+            combination = [outputs[0], outputs[1]]
+            # shape: [batch_size, sequence_len, 2*hidden_num + embedding_dim]
+            final_outputs = tf.concat(combination, 2)  # 将前向和后向的状态连接起来
+            # 增加一个维度作为inchannel用于卷积
+            # shape: [batch_size, sequence_len, 2*hidden_num + embedding_dim， 1]
+            final_outputs = tf.expand_dims(final_outputs, -1, name="final_outputs")
+            # print(final_outputs)
+
+        with tf.name_scope("Conv"):
+            pooling_list = []
+
+            for filter_size in config.filter_sizes:
+
+                # [filter_height, filter_width, in_channels, out_channels]
+                filter_shape = [filter_size, 2*config.hidden_dim, 1, config.filter_num]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[config.filter_num]), name="b")
+
+                # 卷积
+                # [batch_size, sequence_len, width, channels]
+                # output_dim = [batch_size, sequence_len - filter_size + 1, 1, num_filter]
+                # print(W)
+                conv = tf.nn.conv2d(input=final_outputs, filter=W,
+                                    strides=[1, 1, 1, 1], padding="VALID", name="conv")
+                # 换成Relu试试
+                # h = tf.nn.tanh(tf.nn.bias_add(conv, b), name="tanh")
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="tanh")
+
+                # pooling
+                # in_dim = [batch_size, sequence_len - filter_size + 1, 1, num_filter]
+                # out_dim = [batch_size, 1, 1, num_filter]
+                # 这里不使用tf.nn.max_pool是因为pooling的ksize是动态变化的，与输入的文本长度有关
+                pooling = tf.reduce_max(input_tensor=h, axis=1, keep_dims=True)
+                pooling_list.append(pooling)
+        for ex in pooling_list:
+            print(ex)
+        with tf.name_scope("Concat"):
+            total_features = config.filter_num * len(config.filter_sizes)
+            # out_dim [batch_size, 1, 1, total_features]
+            total_pooling = tf.concat(pooling_list, 3)
+            # out_dim [batch_size, total_features]
+            total_pooling = tf.reshape(total_pooling, shape=[-1, total_features], name="total_pooling")
+
+        with tf.name_scope("dropout"):
+            dropout = tf.nn.dropout(total_pooling, keep_prob=self.keep_prob)
+
+        with tf.name_scope("Softmax"):
+            W = tf.get_variable(name="fc_W", shape=[total_features, config.num_classes],
+                                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[config.num_classes]), name='fc_b')
+
+            self.logists = tf.nn.xw_plus_b(dropout, W, b, name="logist")
+            self.prob = tf.nn.softmax(logits=self.logists, name="prob")
+            self.y_pred = tf.argmax(self.prob, 1, name="prediction")
+        with tf.name_scope("Loss"):
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logists, labels=self.input_y)
+            self.l2_loss += tf.nn.l2_loss(W)
+            self.l2_loss += tf.nn.l2_loss(b)
+            self.loss = tf.reduce_mean(cross_entropy) + config.l2_reg_lambda * self.l2_loss
+            self.loss = tf.reduce_mean(cross_entropy)
+
+        with tf.name_scope('optimizer'):
+            self.opt = tf.train.AdamOptimizer(learning_rate=config.learning_rate).minimize(self.loss)
+
+        with tf.name_scope('accuracy'):
+            self.correct = tf.equal(tf.argmax(self.input_y, 1), self.y_pred)
+            self.acc = tf.reduce_mean(tf.cast(self.correct, tf.float32))
+
+    def dropout(self):  # 为每一个rnn核后面加一个dropout层
+        lstm_cell = tf.contrib.rnn.BasicLSTMCell(self.config.hidden_dim, state_is_tuple=True)
+        return tf.contrib.rnn.DropoutWrapper(lstm_cell, output_keep_prob=self.keep_prob)
